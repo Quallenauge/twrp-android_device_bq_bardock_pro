10bd8b501653ae03cc70bad04495fcdd52e5c4be
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2022-04-05 23:00:20 +0800

    mi439: Import stock hvdcp prop for olives

--- recovery/root/system/bin/determine-device.sh
+++ recovery/root/system/bin/determine-device.sh
@@ -13,6 +13,7 @@ case "$(cat /sys/firmware/devicetree/base/model)" in
 	"Olive QRD")
 		set_device_codename "olives"
 		set_device_model "Redmi 8 / 8A / 8A Dual"
+		setprop "persist.vendor.ctm.disallowed" "true"
 		;;
 esac
 
