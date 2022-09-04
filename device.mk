5e4997af00bdde35b2e169520dc29e4b00eec071
me-cafebabe <me_cafebabe@hotmail.com>
2022-09-04 23:09:41 +0800

    onclite: Remove dynamic partitions

--- device.mk
+++ device.mk
@@ -10,10 +10,6 @@ MITHORIUM_INCLUDE_CRYPTO_FBE := true
 # Inherit from mithorium-common
 $(call inherit-product, device/xiaomi/mithorium-common/mithorium.mk)
 
-# Dynamic Partitions
-PRODUCT_USE_DYNAMIC_PARTITIONS := true
-PRODUCT_RETROFIT_DYNAMIC_PARTITIONS := true
-
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
--- recovery/root/system/bin/merge-fstab.sh
+++ recovery/root/system/bin/merge-fstab.sh
@@ -5,21 +5,5 @@ cat /system/etc/recovery.fstab.device >> /system/etc/recovery.fstab
 echo >> /system/etc/twrp.flags
 cat /system/etc/twrp.flags.device >> /system/etc/twrp.flags
 
-# Dynamic Partitions
-if dd if=/dev/block/by-name/system bs=256k count=1|strings|grep mi439_dynpart > /dev/null; then
-    echo >> /system/etc/recovery.fstab
-    for p in system system_ext product vendor odm; do
-        echo "${p} /${p} ext4 rw,barrier=1,discard wait,logical" >> /system/etc/recovery.fstab
-    done
-    echo >> /system/etc/twrp.flags
-    for p in cust vendor; do
-        echo "/super_${p} emmc /dev/block/by-name/${p} flags=display=\"Super_${p}\";backup=1" >> /system/etc/twrp.flags
-    done
-else
-    echo >> /system/etc/twrp.flags
-    cat /system/etc/twrp.flags.nondynpart >> /system/etc/twrp.flags
-    echo "/super emmc /dev/block/by-name/system flags=display=\"Super\";backup=1" >> /system/etc/twrp.flags
-    for p in cust vendor; do
-        echo "/super_${p} emmc /dev/block/by-name/${p} flags=display=\"Super_${p}\";backup=1" >> /system/etc/twrp.flags
-    done
-fi
+echo >> /system/etc/twrp.flags
+cat /system/etc/twrp.flags.nondynpart >> /system/etc/twrp.flags
